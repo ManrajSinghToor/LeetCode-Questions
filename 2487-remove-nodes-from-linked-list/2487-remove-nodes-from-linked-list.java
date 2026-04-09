594c9c01e87d0a5b346612bca8c177c5a4cf32d6
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-04-09 11:51:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -11,23 +11,37 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
         if(head == null) return head;
-
-        Stack<Integer> st = new Stack<>();
+        Stack<ListNode> st = new Stack<>();
         ListNode temp = head;
-        while(temp != null){
-            if(st.isEmpty()) st.push(temp.val);
-            else{
-                while(st.size() > 0 && st.peek() < temp.val) st.pop();
-                if(st.size() == 0 || st.peek() >= temp.val) st.push(temp.val);
+        while (temp != null) {
+            while (!st.isEmpty() && st.peek().val < temp.val) {
+                st.pop();
             }
+            st.push(temp);
             temp = temp.next;
         }
-        ListNode newHead = new ListNode(0);
-        while(!st.isEmpty()) {
-            ListNode newNode = new ListNode(st.pop());
-            newNode.next = newHead.next;
-            newHead.next = newNode;
+        ListNode newHead = null;
+        while (!st.isEmpty()) {
+            ListNode node = st.pop();
+            node.next = newHead;
+            newHead = node;
         }
-        return newHead.next;
+        return newHead;
+        // ListNode temp = head;
+        // while(temp != null){
+        //     if(st.isEmpty()) st.push(temp.val);
+        //     else{
+        //         while(st.size() > 0 && st.peek() < temp.val) st.pop();
+        //         if(st.size() == 0 || st.peek() >= temp.val) st.push(temp.val);
+        //     }
+        //     temp = temp.next;
+        // }
+        // ListNode newHead = new ListNode(0);
+        // while(!st.isEmpty()) {
+        //     ListNode newNode = new ListNode(st.pop());
+        //     newNode.next = newHead.next;
+        //     newHead.next = newNode;
+        // }
+        // return newHead.next;
     }
 }
