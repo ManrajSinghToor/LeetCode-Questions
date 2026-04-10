0ad730ae6deed3109ae4cdd8ff65b22643f3e7ee
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-04-10 18:38:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -11,19 +11,18 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if(head == null || k <= 1) return head;
-
+        int cnt = 0;
         ListNode temp = head;
-        int count = 0;
-        while(temp != null && count < k){
+        while(temp != null && cnt < k){
             temp = temp.next;
-            count++;
+            cnt++;
         }
-        if(count < k) return head;
+        if(cnt < k) return head;
 
-        ListNode forw = null;
-        ListNode curr = head;
         ListNode prev = null;
-        count = 0;
+        ListNode curr = head;
+        ListNode forw = null;
+        int count = 0;
         while(curr != null && count < k){
             forw = curr.next;
             curr.next = prev;
@@ -31,46 +30,7 @@ class Solution {
             curr = forw;
             count++;
         }
-        head.next = reverseKGroup(curr , k);
-
+        head.next = reverseKGroup(curr, k);
         return prev;
     }
 }
-// class Solution {
-//     public ListNode reverseKGroup(ListNode head, int k) {
-//         if(head == null || head.next == null) return head;
-
-//         ListNode temp = head;
-//         int cnt = 0;
-//         while(temp != null){
-//             cnt++;
-//             temp = temp.next;
-//         }
-//         int[] ans = new int[cnt];
-//         temp = head;
-//         for(int i = 0; i < cnt; i++){
-//             ans[i] = temp.val;
-//             temp = temp.next;
-//         }
-
-//         for(int i = 0; i <= cnt - k; i += k){
-//             int left = i;
-//             int right = i + k - 1;
-//             while(left < right){
-//                 int swap = ans[left];
-//                 ans[left] = ans[right];
-//                 ans[right] = swap;
-//                 left++;
-//                 right--;
-//             }
-//         }
-
-//         ListNode dummy = new ListNode(ans[0]);
-//         ListNode curr = dummy;
-//         for(int i = 1; i < cnt; i++){
-//             curr.next = new ListNode(ans[i]);
-//             curr = curr.next;
-//         }
-//         return dummy;
-//     }
-// }
