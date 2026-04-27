ca6d4058a821b322e4fcaa68bd2c887d0ce292ef
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-04-27 18:44:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,7 +19,6 @@ class Solution {
         if(root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        maxCount = Math.max(left, right);
-        return maxCount + 1;
+        return Math.max(left, right) + 1;
     }
 }
