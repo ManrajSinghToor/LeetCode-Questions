2975abe5229d1c4e0b488677b16f4da691819e7a
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-04-29 22:32:55 +0530

    Time: 3 ms (80.79%), Space: 46.7 MB (79.14%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,53 +9,54 @@
  * }
  */
 class Solution {
-    public ListNode mergeLists(ListNode head1, ListNode head2){
+    public ListNode mergeLists(ListNode head1, ListNode head2) {
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy;
 
         ListNode t1 = head1;
         ListNode t2 = head2;
-        while(t1 != null && t2 != null){
-            if(t1.val <= t2.val){
+        while (t1 != null && t2 != null) {
+            if (t1.val <= t2.val) {
                 temp.next = t1;
                 t1 = t1.next;
-            }
-            else {
+            } else {
                 temp.next = t2;
                 t2 = t2.next;
             }
             temp = temp.next;
         }
-        while(t1 != null){
+        while (t1 != null) {
             temp.next = t1;
             t1 = t1.next;
             temp = temp.next;
         }
-        while(t2 != null){
+        while (t2 != null) {
             temp.next = t2;
             t2 = t2.next;
             temp = temp.next;
         }
         return dummy.next;
     }
+
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
+        if (lists == null || lists.length == 0)
+            return null;
 
-        ArrayList<ListNode> list = new ArrayList<>();
-        for(ListNode n : lists){
-            list.add(n);
+        ArrayList<ListNode> arr = new ArrayList<>();
+        for (ListNode n : lists)
+            arr.add(n);
+
+        while (arr.size() > 1) {
+            ArrayList<ListNode> next = new ArrayList<>();
+            for (int i = 0; i < arr.size(); i += 2) {
+                if (i + 1 < arr.size()) {
+                    next.add(mergeLists(arr.get(i), arr.get(i + 1)));
+                } else {
+                    next.add(arr.get(i));
+                }
+            }
+            arr = next;
         }
-
-        while(list.size() > 1){
-            ListNode a = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-
-            ListNode b = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-
-            ListNode c = mergeLists(a, b);
-            list.add(c);
-        } 
-        return list.get(list.size() - 1);
+        return arr.get(0);
     }
 }
