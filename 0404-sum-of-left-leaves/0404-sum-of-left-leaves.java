6b769ae9920cbd7f3e16fd3b7d562daa76ea614e
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-05-05 21:56:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (48.12%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -20,6 +20,7 @@ class Solution {
         if (root == null) return 0;
 
         if (root.left != null && root.left.left == null && root.left.right == null) sum += root.left.val;
+
         sumOfLeftLeaves(root.left);
         sumOfLeftLeaves(root.right);
         return sum;
