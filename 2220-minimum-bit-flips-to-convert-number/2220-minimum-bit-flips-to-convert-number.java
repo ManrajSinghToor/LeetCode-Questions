86702b9a58df15e659a41fcc20cfe11eda7338f5
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-06-29 16:29:19 +0530

    Time: 0 ms (100%), Space: 42.5 MB (5.86%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int mask = start ^ goal;
-        int flips = 0;
+        int diff = start ^ goal;
+        int steps = 0;
         for(int i = 0; i < 31; i++){
-            if((mask >> i) % 2 != 0) flips++;
+            if((diff >> i) % 2 != 0) steps++;
         }
-        return flips;
+        return steps;
     }
 }
