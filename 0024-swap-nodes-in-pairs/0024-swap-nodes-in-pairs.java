e3a73e7be8e5e67251b30645cde6d92b79123ca0
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-03 12:19:54 +0530

    Time: 0 ms (100%), Space: 43.4 MB (11.99%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -9,40 +9,23 @@
  * }
  */
 class Solution {
+    public void  recurs(ListNode prev, ListNode curr){
+        if(curr == null || curr.next == null) return ;
+
+        ListNode f = curr, s = curr.next;
+
+        f.next = s.next;
+        s.next = f;
+        prev.next = s;
+
+        prev = s;
+        recurs(f, f.next);
+    }
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode odd = new ListNode(0);
-        ListNode even = new ListNode(0);
-        ListNode t1 = odd;
-        ListNode t2 = even;
-        ListNode temp = head;
-        int i = 1;
-        while(temp != null){
-            if(i % 2 == 0) {
-                t2.next = temp;
-                t2 = t2.next;
-            }
-            else {
-                t1.next = temp;
-                t1 = t1.next;
-            }
-            i++;
-            temp = temp.next;
-        }
-        t1.next = null;
-        t2.next = null;
-        ListNode ans = new ListNode(-1);
-        ListNode res = ans;
-        t1 = odd.next;
-        t2 = even.next;
-        while(t2 != null){
-            res.next = t2;
-            t2 = t2.next;
-            res = res.next;
-            res.next = t1;
-            t1 = t1.next;
-            res = res.next;
-        }
-        return ans.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        recurs(prev, head);
+        return dummy.next;
     }
 }
