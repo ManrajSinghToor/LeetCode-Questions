c1e121d4c94d7e1e08c5ae44019cb5741000a3f8
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-05 13:40:24 +0530

    Time: 1 ms (100%), Space: 48.1 MB (22.8%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,23 +1,19 @@
 class Solution {
-    public int[] singleNumber(int[] arr) {
-        int[] ans = new int[2];
+    public int[] singleNumber(int[] nums) {
         int xor = 0;
-        for(int i = 0; i < arr.length; i++){
-            xor ^= arr[i];
+        for(int i = 0; i < nums.length; i++){
+            xor ^= nums[i];
         }
-        int mask = ((xor & (xor - 1)) ^ xor);
+        int newXor = (xor & (xor - 1) ^ xor);
         int bucket1 = 0;
         int bucket2 = 0;
-        for(int i = 0; i < arr.length; i++){
-            if((mask & arr[i]) == 0){
-                bucket2 ^= arr[i];
-            }
-            else {
-                bucket1 ^= arr[i];
-            }
+        for(int i = 0; i < nums.length; i++){
+            if((newXor & nums[i]) == 0) bucket1 ^= nums[i];
+            else bucket2 ^= nums[i];
         }
-        ans[0] = bucket1;
-        ans[1] = bucket2;
-        return ans;
+        int[] arr = new int[2];
+        arr[0] = (bucket1 < bucket2) ? bucket1 : bucket2;
+        arr[1] = (bucket1 < bucket2) ? bucket2 : bucket1;
+        return arr;
     }
 }
