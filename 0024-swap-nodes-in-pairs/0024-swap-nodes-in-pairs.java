901d2f1623e0588782b35363689608397ba8e404
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-05 23:27:31 +0530

    Time: 0 ms (100%), Space: 43.2 MB (29%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -9,23 +9,19 @@
  * }
  */
 class Solution {
-    public void  recurs(ListNode prev, ListNode curr){
+    public void recurs(ListNode prev, ListNode curr){
         if(curr == null || curr.next == null) return ;
-
-        ListNode f = curr, s = curr.next;
-
-        f.next = s.next;
-        s.next = f;
-        prev.next = s;
-
-        prev = s;
-        recurs(f, f.next);
+        ListNode f = curr.next;
+        curr.next = f.next;
+        f.next = curr;
+        prev.next = f;
+        recurs(curr, curr.next);
     }
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
-        recurs(prev, head);
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        recurs(dummy, head);
         return dummy.next;
     }
 }
