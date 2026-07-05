cea4732a20d89394ffdee9932c012faa9b66ee90
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-05 10:19:47 +0530

    Time: 30 ms (29.95%), Space: 82.7 MB (78.26%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,32 +1,30 @@
 class Solution {
-    public int partition(int[] arr, int low, int high){
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for(int j = low; j < high; j++){
-            if(arr[j] < pivot){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
-    public void quickSort(int[] arr, int low, int high){
-        if(low < high){
-            int pivot = partition(arr, low, high);
-
-            quickSort(arr, low, pivot - 1);
-            quickSort(arr, pivot + 1, high);
-        }
-    }
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        mergeSort(nums);
         return nums;
+    }
+    public void mergeSort(int[] nums){
+        int n = nums.length;
+        if(n == 1) return ;
+
+        int[] a = new int[n / 2];
+        int[] b = new int[n - n / 2];
+
+        int k = 0;
+        for(int i = 0; i < a.length; i++) a[i] = nums[k++];
+        for(int i = 0; i < b.length; i++) b[i] = nums[k++];
+
+        mergeSort(a);
+        mergeSort(b);
+        merge(nums, a, b);
+    }
+    public void merge(int[] nums, int[] a, int[] b){
+        int i = 0, j = 0, k = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] <= b[j]) nums[k++] = a[i++];
+            else nums[k++] = b[j++];
+        }
+        while(i < a.length) nums[k++] = a[i++];
+        while(j < b.length) nums[k++] = b[j++];
     }
 }
