6055bf57057f03972abbd9609aae8eac512b490e
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-05 11:34:00 +0530

    Time: 0 ms (100%), Space: 43.4 MB (32.91%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -1,21 +1,22 @@
 class Solution {
     public int matrixScore(int[][] grid) {
-        int row = grid.length;
+        int rows = grid.length;
         int cols = grid[0].length;
-        for(int i = 0; i < row; i++){
+        for(int i = 0; i < rows; i++){
             if(grid[i][0] == 0) 
             for(int j = 0; j < cols; j++){
                 grid[i][j] ^= 1;
             }
         }
         for(int i = 0; i < cols; i++){
-            int zeroCnt = 0, onesCnt = 0;
-            for(int j = 0; j < row; j++){
+            int zeroCnt = 0;
+            int onesCnt = 0;
+            for(int j = 0; j < rows; j++){
                 if(grid[j][i] == 0) zeroCnt++;
                 else onesCnt++;
             }
             if(zeroCnt > onesCnt){
-                for(int idx = 0; idx < row; idx++){
+                for(int idx = 0; idx < rows; idx++){
                     grid[idx][i] ^= 1;
                 }
             }
@@ -23,7 +24,7 @@ class Solution {
         int sum = 0, pow = 1;
         for(int i = cols - 1; i >= 0; i--){
             int ones = 0;
-            for(int j = 0; j < row; j++){
+            for(int j = 0; j < rows; j++){
                 if(grid[j][i] == 1) ones++;
             }
             sum += pow * ones;
