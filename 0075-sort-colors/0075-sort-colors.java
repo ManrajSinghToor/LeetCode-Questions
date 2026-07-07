47e4c1cd4066299960a02e642250c3341b36578d
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-07 12:51:18 +0530

    Time: 0 ms (100%), Space: 43 MB (98.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,35 +1,34 @@
 class Solution {
-    public void merge(int[] a, int[] b, int[] arr){
-        int i = 0, j = 0, k = 0;
-        while(i < a.length && j < b.length){
-            if(a[i] <= b[j]) arr[k++] = a[i++];
-            else arr[k++] = b[j++];
-        }
-        while(i < a.length){
-            arr[k++] = a[i++];
-        }
-        while(j < b.length){
-            arr[k++] = b[j++];
-        }
-    }
-    public void mergeSort(int[] arr){
-        int n = arr.length;
-        if(n == 1) return;
-        int[] a = new int[n/2];
-        int[] b = new int[n-n/2];
-        int idx = 0;
-        for(int i = 0; i < a.length; i++){
-            a[i] = arr[idx++];
-        }
-        for(int i = 0; i < b.length; i++){
-            b[i] = arr[idx++];
-        }
-        mergeSort(a);
-        mergeSort(b);
-
-        merge(a, b, arr);
-    }
     public void sortColors(int[] nums) {
-        mergeSort(nums);
+        int c0 = 0, c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0) c0++;
+            else if(nums[i] == 1) c1++;
+            else c2++;
+        }
+        int i = 0;
+        while(i < nums.length){
+            if(c0 != 0){
+                while(c0 != 0) {
+                    nums[i] = 0; 
+                    c0--;
+                    i++;
+                }
+            }
+            else if(c1 != 0){
+                while(c1 != 0){
+                    nums[i] = 1;
+                    c1--;
+                    i++;
+                }
+            }
+            else {
+                while(c2 != 0){
+                    nums[i] = 2;
+                    c2--;
+                    i++;
+                }
+            }
+        } 
     }
 }
