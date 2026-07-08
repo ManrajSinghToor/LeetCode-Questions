179dd6dba1d791311eca3d443d85ee4e2c1879e3
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-09 00:14:09 +0530

    Time: 21 ms (23.3%), Space: 56.6 MB (20.75%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] nums, int limit) {
+        Arrays.sort(nums);
+        int i = 0; 
+        int j = nums.length - 1;
+        int boats = 0;
+        while(i <= j){
+            if(nums[i] + nums[j] <= limit){
+                boats++;
+                i++;
+                j--;
+            }
+            else {
+                boats++;
+                j--;
+            }
+        }
+        return boats;
+    }
+}
