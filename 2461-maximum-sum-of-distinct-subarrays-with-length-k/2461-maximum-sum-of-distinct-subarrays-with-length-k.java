f551f1dd40116e87751af237e45de3ea20bed129
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-10 10:41:49 +0530

    Time: 83 ms (5.48%), Space: 104.6 MB (6.09%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,38 +1,35 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int i = 0;
         long max = 0;
         long sum = 0;
-        int duplicate = 0;
-        while(i < k){
-            if(!map.containsKey(nums[i])) map.put(nums[i], 0);
-            map.put(nums[i], map.get(nums[i]) + 1);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int isDup = 0;
+
+        for(int i = 0; i < k; i++){
+            if(map.containsKey(nums[i])) map.put(nums[i], map.get(nums[i]) + 1);
+            else map.put(nums[i], 1);
 
             sum += nums[i];
 
-            if(map.get(nums[i]) > 1) duplicate++;
-            i++;
+            if(map.get(nums[i]) > 1) isDup++;
         }
+        if(isDup == 0) max = Math.max(sum, max);
 
-        if(duplicate == 0) max = Math.max(max, sum);
-
-        for(int idx = k; idx < nums.length; idx++){
-            int numToAdd = nums[idx];
-            int numToRemove = nums[idx - k];
-            
-            if(!map.containsKey(numToAdd)) map.put(numToAdd, 0);
-            map.put(numToAdd, map.get(numToAdd) + 1);
-
-            if(map.get(numToAdd) > 1) duplicate++;
+        for(int i = k; i < nums.length; i++){
+            int numToAdd = nums[i];
+            int numToRemove = nums[i - k];
+            if(map.containsKey(numToAdd)) map.put(numToAdd, map.get(numToAdd) + 1);
+            else map.put(numToAdd, 1);
 
             sum += numToAdd;
-            if(map.get(numToRemove) > 1) duplicate--;
+            if(map.get(numToAdd) > 1) isDup++;
+
+            if(map.get(numToRemove) > 1) isDup--;
             map.put(numToRemove, map.get(numToRemove) - 1);
 
             sum -= numToRemove;
-
-            if(duplicate == 0) max = Math.max(max, sum);
+            if(isDup == 0) max = Math.max(sum, max);
         }
         return max;
     }
