7d8022af79e6eefc1f4988d8b27d2e1ddf9cce72
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-14 23:35:00 +0530

    Time: 4 ms (100%), Space: 225.7 MB (73.85%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,50 +10,22 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        // ListNode dummy = new ListNode(0);
-        // ListNode temp = dummy;
+        ListNode curr = head.next;
+        ListNode sumKaro = curr;
 
-        // ListNode first = head;
-
-        // while(first.next != null) {
-        //     ListNode second = first.next;
-        //     while(second != null && second.val != 0) second = second.next;
-
-        //     int sum = 0;
-        //     ListNode sumKaro = first.next;
-        //     while(sumKaro != second){
-        //         sum += sumKaro.val;
-        //         sumKaro = sumKaro.next;
-        //     }
-
-        //     if(sum > 0){
-        //         ListNode newNode = new ListNode(sum);
-        //         temp.next = newNode;
-        //         temp = temp.next;
-        //     }
-        //     first = second;
-        // }
-        // return dummy.next;
-        ListNode modify = head.next;
-        ListNode nextSum = modify;
-
-        while (nextSum != null) {
+        while (sumKaro != null) {
 
             int sum = 0;
-
-            while (nextSum.val != 0) {
-                sum += nextSum.val;
-                nextSum = nextSum.next;
+            while (sumKaro.val != 0) {
+                sum += sumKaro.val;
+                sumKaro = sumKaro.next;
             }
 
-            modify.val = sum;
-
-            nextSum = nextSum.next;
-            modify.next = nextSum;
-
-            modify = nextSum;
+            curr.val = sum;
+            sumKaro = sumKaro.next;
+            curr.next = sumKaro;
+            curr = sumKaro;
         }
-
         return head.next;
     }
 }
