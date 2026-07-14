d08f75be696126eb43993f56993483873dc36102
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-14 11:30:03 +0530

    Time: 36 ms (6.3%), Space: 53.8 MB (93.78%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,17 +1,32 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> set = new HashSet<>();
-        List<String> list = new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
 
-        int j = 0;
-        while (j <= s.length() - 10) {
-            String ans = s.substring(j, j + 10);
-
-            if (set.contains(ans) && !list.contains(ans)) list.add(ans);
-            set.add(ans);
-
-            j++;
+        int k = 10;
+        int val = 0;
+        if (s.length() < k) return new ArrayList<>();
+        
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+        for(int i = 0; i < k; i++){
+            int pow = k - i - 1;
+            val += (int)(Math.pow(4, pow) * map.get(s.charAt(i)));
         }
-        return list;
+
+        set1.add(val);
+
+        for(int i = k; i < s.length(); i++){
+            val -= (int)(Math.pow(4, k - 1) * map.get(s.charAt(i - k)));
+            val *= 4;
+            val += map.get(s.charAt(i));
+
+            if(set1.contains(val)) set2.add(s.substring(i - k + 1, i + 1));
+            set1.add(val);
+        }
+        return new ArrayList<String>(set2);
     }
 }
