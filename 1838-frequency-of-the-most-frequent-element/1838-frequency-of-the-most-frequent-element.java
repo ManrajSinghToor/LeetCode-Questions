9d704d74d6ad85b56ce77e87bf1e5a1f46ab195b
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-16 22:40:00 +0530

    Time: 33 ms (92.69%), Space: 94.6 MB (81.76%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -16,15 +16,5 @@ class Solution {
             
         }
         return max;
-        // for(int i = 0; i < nums.length; i++){
-        //     int sum = 0;
-        //     for(int j = i; j < nums.length; j++){
-        //         sum += nums[j];
-        //         int total = nums[j] * (j - i + 1);
-        //         int x = total - sum;
-        //         if(x > k) break;
-        //         max = Math.max(max, (j - i + 1));
-        //     }
-        // }
     }
 }
