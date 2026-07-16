4da0d4de9f490bb625c2e50b02e5dd1293c0fd7b
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-16 22:37:41 +0530

    Time: 33 ms (92.69%), Space: 94.5 MB (91.12%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int max = 0;
+        int i = 0;
+        int j = 0;
+        long sum = 0;
+        while(j < nums.length){
+            sum += nums[j];
+            while((long) nums[j] * (j - i + 1) - sum > k){
+                sum -= nums[i];
+                i++;
+            }
+            max = Math.max(max, (j - i + 1));
+            j++;
+            
+        }
+        return max;
+        // for(int i = 0; i < nums.length; i++){
+        //     int sum = 0;
+        //     for(int j = i; j < nums.length; j++){
+        //         sum += nums[j];
+        //         int total = nums[j] * (j - i + 1);
+        //         int x = total - sum;
+        //         if(x > k) break;
+        //         max = Math.max(max, (j - i + 1));
+        //     }
+        // }
+    }
+}
