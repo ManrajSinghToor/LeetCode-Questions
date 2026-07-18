c691ca0f1869e0b2910e351938522acb394a26bb
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-18 13:01:33 +0530

    Time: 4 ms (74.55%), Space: 45.1 MB (70.5%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -6,12 +6,8 @@ class Solution {
             while(st.size() > 0 && st.peek() < nums2[i]){
                 st.pop();
             }
-            if(st.size() > 0){
-                map.put(nums2[i], st.peek());
-            }
-            else {
-                map.put(nums2[i], -1);
-            }
+            if(st.size() > 0) map.put(nums2[i], st.peek());
+            else map.put(nums2[i], -1);
             st.push(nums2[i]);
         }
         for(int i = 0; i < nums1.length; i++){
