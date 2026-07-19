317adb94e1e553f6b64411c4905852bbdd75f557
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-19 11:11:24 +0530

    Time: 0 ms (100%), Space: 46.4 MB (72.42%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,74 +16,70 @@ class Node {
 class Solution {
     public Node deepCopy(Node head){
         Node dummy = new Node(0);
-        Node t1 = dummy;
-        Node temp = head;
-        while(temp != null){
-            Node newNode = new Node(temp.val);
-            t1.next = newNode;
-            t1 = t1.next;
+        Node temp = dummy;
+
+        Node curr = head;
+        while(curr != null){
+            Node newNode = new Node(curr.val);
+            temp.next = newNode;
+            curr = curr.next;
             temp = temp.next;
         }
-        t1.next = null;
+        temp.next = null;
         return dummy.next;
     }
     public void alterConnect(Node head1, Node head2){
-        Node d1 = new Node(-1);
-        Node t1 = d1;
-        
-        Node temp = head1;
-        Node temp2 = head2;
-        while(temp2 != null){
-            t1.next = temp;
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+
+        Node t1 = head1;
+        Node t2 = head2;
+        while(t2 != null){
+            temp.next = t1;
             t1 = t1.next;
             temp = temp.next;
-            t1.next = temp2;
-            t1 = t1.next;
-            temp2 = temp2.next;
+            temp.next = t2;
+            t2 = t2.next;
+            temp = temp.next;
         }
     }
     public void assignRandom(Node head1, Node head2){
         Node t1 = head1;
         Node t2 = head2;
-
         while(t1 != null && t2 != null){
             if(t1.random == null) t2.random = null;
-            else t2.random = t1.random.next;
+            else  t2.random = t1.random.next;
             t1 = t1.next.next;
             if(t2.next != null) t2 = t2.next.next;
         }
     }
-    public void MakeOriginal(Node head1, Node head2){
+    public void makeOriginal(Node head1, Node head2){
         Node ansDummy = new Node(0);
-        Node ansTemp = ansDummy;
-
         Node fakeDummy = new Node(-1);
-        Node fakeTemp = fakeDummy;
 
-        Node t1 = head1;
-        Node t2 = head2;
-        while(t1 != null && t2 != null){
-            fakeTemp.next = t1;
-            t1 = t1.next.next;
-            fakeTemp = fakeTemp.next;
-            ansTemp.next = t2;
-            if(t2.next != null) t2 = t2.next.next;
-            ansTemp = ansTemp.next;
+        Node t1 = fakeDummy;
+        Node t2 = ansDummy;
+
+        Node curr1 = head1;
+        Node curr2 = head2;
+        while(curr1 != null && curr2 != null){
+            t1.next = curr1;
+            t1 = t1.next;
+            curr1 = curr1.next.next;
+            t2.next = curr2;
+            t2 = t2.next;
+            if(curr2.next != null) curr2 = curr2.next.next;
         }
-        fakeTemp.next = null;
+        t1.next = null;
     }
     public Node copyRandomList(Node head) {
-        // Step 1. -> Make Simple deepCopy of given List 
-        Node newHead = deepCopy(head);
+        Node head2 = deepCopy(head);
 
-        //Step 2. -> Make Alternate connections b/w two lists
-        alterConnect(head, newHead);
+        alterConnect(head, head2);
 
-        //Step 3. -> Assign Random Correctly
-        assignRandom(head, newHead);
+        assignRandom(head, head2);
 
-        //Step 4. -> Make the original list remove alternate connections
-        MakeOriginal(head, newHead);
-        return newHead;
+        makeOriginal(head, head2);
+        return head2;
     }
 }
