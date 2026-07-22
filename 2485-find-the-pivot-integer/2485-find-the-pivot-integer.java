d6a1dc8594ac9c5335992e19fe598de5635263de
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-07-22 23:11:09 +0530

    Time: 1 ms (70.66%), Space: 42.5 MB (23.81%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) sum += i;
+
+        int left = 0;
+        for(int i = 1; i <= n; i++){
+            int right = sum - left;
+
+            if(left + i == right) return i;
+            left += i;
+        }
+        return -1;
+    }
+}
