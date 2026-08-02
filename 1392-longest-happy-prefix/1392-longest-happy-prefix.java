e3c7bbf389835b269dcca824dda6f9d945776b62
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-02 16:37:00 +0530

    Time: 17 ms (7.07%), Space: 47.7 MB (60.17%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.java
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -1,18 +1,32 @@
 class Solution {
-    public String longestPrefix(String s) {
-        int n = s.length();
+    public int[] buildLps(String s, int n){
         int[] lps = new int[n];
-        int i = 1, len = 0;
+        int len = 0;
+        int i = 1;
         while(i < n){
             if(s.charAt(i) == s.charAt(len)){
                 len++;
-                lps[i++] = len;
+                lps[i] = len;
+                i++;
             }
-            else {
-                if(len != 0) len = lps[len - 1];
-                else lps[i++] = 0;
+            else { 
+                if(len > 0){
+                    len = lps[len - 1];
+                }
+                else i++;
             }
         }
-        return s.substring(0, lps[n - 1]);
+        return lps;
+    }
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = buildLps(s, n);
+
+        int lastLps = lps[n - 1];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < lastLps; i++){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }
