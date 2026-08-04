66660d1dd0ec37b9676758da6c4d16e68a3d5948
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-04 09:26:41 +0530

    Time: 3 ms (81.76%), Space: 46.8 MB (53.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -4,14 +4,15 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             min = Math.min(nums[i], min);
             max = Math.max(nums[i], max);
             set.add(nums[i]);
         }
 
-        for(int i = min; i < max; i++){
-            if(!set.contains(i)) ans.add(i);
+        for (int i = min; i < max; i++) {
+            if (!set.contains(i))
+                ans.add(i);
         }
         return ans;
     }
