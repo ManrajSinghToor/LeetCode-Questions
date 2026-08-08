34434c19da9aecded93b25787fe4e71d6e657cdd
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-08 12:32:46 +0530

    Time: 21 ms (87.28%), Space: 47.4 MB (81.62%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,25 +1,31 @@
 class Solution {
     public String sortVowels(String s) {
         StringBuilder sb = new StringBuilder(s);
-        List<Character> list = new ArrayList<>();
+        int vowels = 0;
         for(int i = 0; i < sb.length(); i++){
             char c = sb.charAt(i);
-            if(c == 'a'|| c == 'e'|| c =='i'|| c == 'o'|| c == 'u'
-            || c == 'A'|| c == 'I'|| c =='O'|| c == 'E'|| c == 'U'){
-                list.add(c);
+            if(c == 'a' || c == 'i' || c == 'e' || c == 'u' || c == 'o' 
+            || c == 'A' || c == 'I' || c == 'E' || c == 'U' || c == 'O') {
+                vowels++;
+            }
+        }
+        char[] arr = new char[vowels];
+        int idx = 0;
+        for(int i = 0; i < sb.length(); i++){
+            char c = sb.charAt(i);
+            if(c == 'a' || c == 'i' || c == 'e' || c == 'u' || c == 'o' 
+            || c == 'A' || c == 'I' || c == 'E' || c == 'U' || c == 'O') {
+                arr[idx++] = c;
                 sb.setCharAt(i, '#');
             }
         }
-        if(list.size() == 0) return s;
-        Collections.sort(list);
-        int idx = 0;
-        int i = 0;
-        while(i < sb.length()){
+        Arrays.sort(arr);
+        int k = 0;
+        for(int i = 0; i < sb.length(); i++){
             char ch = sb.charAt(i);
-            if(idx < list.size() && ch == '#') {
-                sb.setCharAt(i, list.remove(idx));
+            if(ch == '#'){
+                sb.setCharAt(i, arr[k++]);
             }
-            i++;
         }
         return sb.toString();
     }
