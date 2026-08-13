cd62163425936d981417eaa957225ba8545d40fc
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-13 17:13:34 +0530

    Time: 1 ms (60.5%), Space: 42.7 MB (63.87%) - LeetHub

---
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minTimeToType(String word) {
+        int time = 0;
+        char curr = 'a';
+        int i = 0;
+        while(i < word.length()) {
+            int diff = Math.abs(word.charAt(i) - curr);
+            time += Math.min(diff, 26 - diff);
+            time++;
+            curr = word.charAt(i);
+            i++;
+        }
+        return time;
+    }
+}
