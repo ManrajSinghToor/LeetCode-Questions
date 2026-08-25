f38ab1561cab1c9b2e010c545e2d104cc39c5147
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-25 12:02:00 +0530

    Time: 19 ms (34.72%), Space: 50.8 MB (69.22%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,15 +1,34 @@
+class Triplet implements Comparable<Triplet> {
+    int ele;
+    int r;
+    int c;
+    Triplet(int ele, int r, int c) {
+        this.ele = ele;
+        this.r = r;
+        this.c = c;
+    }
+    public int compareTo(Triplet t) {
+        return this.ele - t.ele;
+    }
+}
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        if(matrix.length * matrix[0].length <= 1) return matrix[0][0];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                pq.add(matrix[i][j]);
+        //T.C - O(n log k) S.C - O(k);
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        for(int i = 0; i < matrix.length; i++) {
+            pq.add(new Triplet(matrix[i][0], i, 0));
+        }
+        while(k > 1) {
+            Triplet top = pq.remove();
+            int row = top.r;
+            int col = top.c;
+
+            if(col + 1 < matrix[0].length) {
+                pq.add(new Triplet(matrix[row][col + 1], row, col + 1));
             }
+            k--;
         }
-        while(k-- > 1){
-            pq.remove();
-        }
-        return pq.peek();
+        Triplet top = pq.remove();
+        return top.ele;
     }
 }
