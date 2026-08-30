706ac7e57a30f1ef32043dc0b1624ccbdc459039
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-08-30 11:19:19 +0530

    Time: 1 ms (95.79%), Space: 46.4 MB (29.25%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -1,24 +1,23 @@
 class Solution {
-    public boolean recur(int[][] arr, int row, int cols, int sum){
-        int n = arr.length;
+    public boolean recurs(int row, int col, int sum, int[][] grid) {
+        int n = grid.length;
+        if(grid[0][0] != 0) return false;
         int[][] moves = {
-            {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
-            {1, 2}, {1, -2}, {-1, 2}, {-1, -2},
+            {2, -1}, {2, 1}, {1, 2}, {-1, 2},
+            {-2, -1}, {-2, 1}, {-1, -2}, {1, -2}
         };
         if(sum == n * n - 1) return true;
+        for(int[] move : moves) {
+            int r = row + move[0];
+            int c = col + move[1];
 
-        for(int[] m : moves){
-            int nr = row + m[0];
-            int nc = cols + m[1];
-
-            if(nr >= 0 && nr < n && nc >= 0 && nc < n && arr[nr][nc] == sum + 1){
-                return recur(arr, nr, nc, sum + 1);
+            if(r >= 0 && r < n && c >= 0 && c < n && grid[r][c] == sum + 1) {
+                return recurs(r, c, sum + 1, grid);
             }
         }
         return false;
     }
     public boolean checkValidGrid(int[][] grid) {
-        if(grid[0][0] != 0) return false;
-        return recur(grid, 0, 0, 0);
+        return recurs(0, 0, 0, grid);
     }
 }
