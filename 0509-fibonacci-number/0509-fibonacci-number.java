5be39bbe747a42fdcf1ceb9345eab0c6cbf1989a
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-04 19:50:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int fibo(int n, int[] dp) {
         if(n <= 1) return n;
         if(dp[n] != 0) return dp[n];
-        int ans = fib(n - 1) + fib(n - 2);
+        int ans = fibo(n - 1, dp) + fibo(n - 2, dp);
         dp[n] = ans;
         return ans;
     }
