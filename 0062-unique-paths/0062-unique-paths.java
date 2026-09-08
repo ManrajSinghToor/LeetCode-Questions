b3d47d1ee162747711433c1c5ab2a118ce08eaab
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-08 21:49:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,17 @@
 class Solution {
-    static int[][] dp;
-    public int uniPath(int st1, int st2, int end1, int end2){
-        if(st1 == end1 && st2 == end2) return 1;
-        if(st1 < 0 || st1 > end1 || st2 < 0 || st2 > end2) return 0;
-        if(dp[st1][st2] != -1) return dp[st1][st2];
-        int down = uniPath(st1 + 1, st2, end1, end2);
-        int right = uniPath(st1, st2 + 1, end1, end2);
-        int ans = down + right;
-        dp[st1][st2] = ans;
-        return ans;
-    }
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
-        return uniPath(0, 0, m - 1, n - 1);
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
