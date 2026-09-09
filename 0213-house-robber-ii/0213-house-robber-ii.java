a22694b46f337a29ef936433083e30d8db2adbb3
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-09 13:24:24 +0530

    Time: 0 ms (100%), Space: 42.9 MB (22.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,21 +1,21 @@
 class Solution {
-    static int[] dp;
-    public int chor(int idx, int n, int[] nums) {
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int pick = nums[idx] + chor(idx + 2, n, nums);
-        int skip = chor(idx + 1, n, nums);
-        int ans = Math.max(pick, skip);
-        dp[idx] = ans;
-        return ans;
+    public int chorBc(int st, int end, int[] dp, int[] nums) {
+        if(st == end ) return nums[st];
+        dp[st] = nums[st];
+        dp[st + 1] = Math.max(nums[st], nums[st + 1]);
+        for(int i = st + 2; i <= end; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        return dp[end];
     }
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        int ans1 = chor(0, nums.length - 1, nums);
-        Arrays.fill(dp, -1);
-        int ans2 = chor(1, nums.length, nums);
+        int n = nums.length;
+        if(n == 1) return nums[0];
+
+        int[] dp = new int[n];
+        int ans1 = chorBc(0, n - 2, dp, nums);
+        int ans2 = chorBc(1, n - 1, dp, nums);
+
         return Math.max(ans1, ans2);
     }
 }
