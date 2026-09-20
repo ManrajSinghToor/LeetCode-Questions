1e350334fd1f1076d94e8ebe3fb15015545224b0
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-20 11:19:17 +0530

    Time: 108 ms (37.51%), Space: 280.8 MB (5.37%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,22 @@
 class Solution {
-    int count = 0;
-    public void recurs(int[] nums, int idx, int sum, int target){
-        if(idx == nums.length){
-            if(sum == target){
-                count++;
-            }
-            return;
+    static int[][] dp;
+    static int absSum;
+    public int targetSum(int idx, int sum, int[] nums, int target) {
+        if(idx == nums.length) {
+            if(sum == target) return 1;
+            else return 0;
         }
-        recurs(nums, idx + 1, sum + nums[idx], target);
-
-        recurs(nums, idx + 1, sum - nums[idx], target);
+        if(dp[idx][sum+absSum] != -1) return dp[idx][sum+absSum]; 
+        int add = targetSum(idx + 1, sum + nums[idx], nums, target);
+        int sub = targetSum(idx + 1, sum - nums[idx], nums, target);
+        int ans = add + sub;
+        dp[idx][sum+absSum] = ans;
+        return ans;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        recurs(nums, 0, 0, target);
-        return count;
+        for(int ele : nums) absSum += Math.abs(ele);
+        dp = new int[nums.length][2 * absSum + 1];
+        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
+        return targetSum(0, 0, nums, target);
     }
 }
