0d0267b8dd27d40bf24ab1c7e0bd72446831e90c
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-20 09:45:27 +0530

    Time: 40 ms (7.75%), Space: 47.8 MB (6.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,28 @@
 class Solution {
     static int[][] dp;
-    public int coin(int idx, int[] coins, int amount) {
+    public int coins(int idx, int[] coins, int amt) {
         if(idx == coins.length) {
-            if(amount == 0) return 0;
+            if(amt == 0) return 0;
             else return Integer.MAX_VALUE;
         }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
+        if(dp[idx][amt] != -1) return dp[idx][amt];
         int pick = 0;
-        if(coins[idx] <= amount) {
-            int help = coin(idx, coins, amount - coins[idx]);
-            if(help != Integer.MAX_VALUE) pick = 1 + help;
-            else pick = help;
+        if(amt >= coins[idx]) {
+            int recurs = coins(idx, coins, amt - coins[idx]);
+            if(recurs != Integer.MAX_VALUE) pick = 1 + recurs;
+            else pick = recurs;
         }
-        int skip = coin(idx + 1, coins, amount);
-        if(coins[idx] > amount) return skip;
+        int skip = coins(idx + 1, coins, amt);
+        if(amt < coins[idx]) return skip;
         int ans = Math.min(pick, skip);
-        dp[idx][amount] = ans;
+        dp[idx][amt] = ans;
         return ans;
+
     }
     public int coinChange(int[] coins, int amount) {
         dp = new int[coins.length][amount + 1];
         Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        int ans = coin(0, coins, amount);
+        int ans = coins(0, coins, amount);
         if(ans == Integer.MAX_VALUE) return -1;
         else return ans;
     }
