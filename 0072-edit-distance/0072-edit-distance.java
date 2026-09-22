2e8ae092cc2ac45eb8a0f7dbadc71bf8b46a2f1a
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2026-09-22 11:48:30 +0530

    Time: 5 ms (66.1%), Space: 47.1 MB (52.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,22 @@
 class Solution {
-    static int[][] dp;
-    public int dist(int i, int j, StringBuilder sb1, StringBuilder sb2) {
-        if(j < 0) return i + 1;
-        if(i < 0) return j + 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int ans = 0;
-        if(sb1.charAt(i) == sb2.charAt(j)) {
-            return dist(i - 1, j - 1, sb1, sb2);
-        }
-        else {
-            int replace = 1 + dist(i - 1, j - 1, sb1, sb2);
-            int delete = 1 + dist(i - 1, j, sb1, sb2);
-            int insert = 1 + dist(i, j - 1, sb1, sb2);
-            ans = Math.min(insert, Math.min(delete, replace));
-        }
-        dp[i][j] = ans;
-        return ans;
-    }
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        dp = new int[n][m];
-        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        return dist(n - 1, m - 1, new StringBuilder(word1), new StringBuilder(word2));
+        int[][] dp = new int[n + 1][m + 1];
+        
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    int replace = dp[i - 1][j - 1];
+                    int delete = dp[i - 1][j];
+                    int insert = dp[i][j - 1];
+                    dp[i][j] = 1 + Math.min(replace, Math.min(delete, insert));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
